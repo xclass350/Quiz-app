209df853db013dea1ce0547f6d232469cf4a2d0e
xclass350 <156424219+xclass350@users.noreply.github.com>
2025-02-03 18:57:58 +0100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,48 @@
+let questions = [
+  {
+    numb: 1,
+    question: "What is the output of typeof null?",
+    answer: "object",
+    options: ["object", "null", "undefined", "string"],
+  },
+
+  {
+    numb: 2,
+    question:
+      "Which JavaScript function is used to convert a string to an integer?",
+    answer: "parseInt()",
+    options: ["parseInt()", "parseFloat()", "toString()", "valueOf()"],
+  },
+
+  {
+    numb: 3,
+    question: "What is the purpose of the this keyword in JavaScript?",
+    answer: "To refer to the current object",
+    options: [
+      "Hypertext Preprocessor",
+      "To refer to the current object",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor",
+    ],
+  },
+
+  {
+    numb: 4,
+    question:
+      "Which JavaScript method is used to add an element to the end of an array?",
+    answer: "push()",
+    options: ["push()", "pop()", "shift()", "unshift()"],
+  },
+
+  {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language",
+    ],
+  },
+];
